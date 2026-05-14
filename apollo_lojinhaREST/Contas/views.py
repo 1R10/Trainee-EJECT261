3bbc55b2eaf3df4712ef83071f0c15b8da963d6a
1R10 <ryanguilherme2003@gmail.com>
2026-05-14 13:05:42 -0300

    feat:  add modelo base json e url para o mesmo

--- apollo_lojinhaREST/Contas/views.py
+++ apollo_lojinhaREST/Contas/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.http import JsonResponse
+# Aqui gero os meus http's. Perguntar o motivo de não usar o https já que https>http. Concluir a trilha antes para sanar a dúvida nela se possível
+# Lembrar de enviar pras URL's na pasta mãe
+def Contas(request):
+    conta = {
+        'id': '1',
+        'nome': 'Ryan'
+    }
+    return JsonResponse(Contas)
