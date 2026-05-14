3b95e7c765e4b56c3ce5e623ada8186f7a556c32
1R10 <ryanguilherme2003@gmail.com>
2026-05-14 15:18:38 -0300

    feat:  novos parâmetros na view Contas

--- apollo_lojinhaREST/Contas/models.py
+++ apollo_lojinhaREST/Contas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Cadastrar_user(models.Model):
+    nome_completo = models.CharField(max_length=100, blank= False)
+    nascimento    = models.DateField(blank= False)
+    cpf           = models.CharField(max_length=11, blank= False)
+    email         = models.EmailField(max_length=100, blank= False)
+    senha         = models.TextField(max_length=50, blank= False) # min 8 car, 1 maiuscula 1 especial
+    telefone      = models.TextField(max_length=14, blank= False) 
+    cep           = models.TextField(max_length=10, blank= False)
+    endereco      = models.TextField( blank= False)
+
+def __str__(self):
+    return self.nome_completo
--- apollo_lojinhaREST/Contas/views.py
+++ apollo_lojinhaREST/Contas/views.py
@@ -3,7 +3,13 @@ from django.http import JsonResponse
 # Lembrar de enviar pras URL's na pasta mãe
 def Contas(request):
     conta = {
-        'id': '1',
-        'nome': 'Ryan'
+        'id': '0',
+        'nome_completo': 'nome',
+        'nascimento':    'dd/mm/aaaa',
+        'cpf':           '12345678910',
+        'email':         'tengotelengo@tengoede.carrapixo',
+        'senha':         'segredoveinaoexplana',
+        'cep':           '0000000000',
+        'endereco':      ['rua', 'numero','bairro','cidade','estado','pais','continente'] # isso ta ok?
     }
     return JsonResponse(conta)
