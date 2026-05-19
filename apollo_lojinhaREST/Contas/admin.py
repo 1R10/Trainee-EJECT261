0379e66db5e9e05ddd1afa6cb14773f22278bca0
1R10 <ryanguilherme2003@gmail.com>
2026-05-19 00:15:57 -0300

    feat: Corriji os users.

--- apollo_lojinhaREST/Contas/admin.py
+++ apollo_lojinhaREST/Contas/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.models import AbstractUser
 from Contas.models import ContaPadrao
 
-'''class CadastrarUserAdmin(admin.ModelAdmin):
+class ContaPadraoAdmin(admin.ModelAdmin):
     list_display = ('role',
                     'nome_completo', 
                     'nascimento', 
                     'cpf',
-                    'email', 
-                    'senha', 
+                    #'email', 
+                    #'senha', 
                     'telefone', 
                     'cep', 
                     'endereco', )
@@ -17,6 +16,4 @@ from Contas.models import ContaPadrao
     list_per_page      = 20
     search_fields      = ('nome_completo',)
 
-admin.site.register(ContaPadrão, CadastrarUserAdmin)'''
-
-admin.site.register(ContaPadrao)
+admin.site.register(ContaPadrao,ContaPadraoAdmin)
--- apollo_lojinhaREST/Contas/models.py
+++ apollo_lojinhaREST/Contas/models.py
@@ -8,6 +8,17 @@ class ContaPadrao(AbstractUser):
         ('C', 'Cliente'),
     )
     role          = models.TextField(choices=ROLE, default='C')
+    nome_completo = models.CharField(max_length=100, blank= False)
+    nascimento    = models.DateField(blank= False)
+    cpf           = models.CharField(max_length= 11, blank= False)
+    #email         = models.EmailField(max_length= 100, blank= False)
+    #senha         = models.TextField(max_length= 50, blank= False) # min 8 car, 1 maiuscula 1 especial
+    telefone      = models.TextField(max_length= 14, blank= False) 
+    cep           = models.TextField(max_length= 10, blank= False)
+    endereco      = models.TextField( blank= False)
+    REQUIRED_FIELDS = ['nome_completo', 'nascimento', 'cpf']
+    def __str__(self):
+        return self.nome_completo
   
 
 
