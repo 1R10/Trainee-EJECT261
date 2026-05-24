e501e648e97cec2aec8eb9ec3e685d8de5a0b386
1R10 <ryanguilherme2003@gmail.com>
2026-05-24 18:53:41 -0300

    feat: add model para produto de carrinho. assim, ao deletar o produto o carrinho se mantém. até no histórico (Espero)

--- apollo_lojinhaREST/Carrinho/models.py
+++ apollo_lojinhaREST/Carrinho/models.py
@@ -3,10 +3,25 @@ from Contas.models import ContaPadrao
 from Produtos.models import Produtos
 class Carrinho(models.Model):
     ESTADO = (
-        ('A', 'Aberto'),
-        ('F', 'Fechado'),
-        ('C', 'Cancelado')
+        ('A', 'Aberto'),   # Carrinho atual
+        ('F', 'Fechado'),  # Histórico de compras
+        ('C', 'Cancelado') # Cancelado vou deletar do banco
     )
-    usuario = models.ForeignKey(ContaPadrao, on_delete=models.OneToOneField) # Usuário deletado, carrinho também.
-    item    = models.TextField()# como dadano eu associo?
-    estado  = models.TextField(choices=ESTADO, default='A')
+    usuario = models.ForeignKey(ContaPadrao, on_delete=models.CASCADE, verbose_name='UsuárioCarrinho') # Usuário deletado, carrinho também.
+    estado  = models.CharField(choices=ESTADO, default='A', max_length=1, verbose_name='EstadoCarrinho')
+    carrinhoData = models.DateField(auto_now_add=True, verbose_name='InicioCarrinho') # Data de criação do carrinho
+
+
+
+    # Optei por fazer os itens a parte. O CASCADE deletaria tudo, então eu preciso de um modelo APENAS para o item do carrinho?
+    # Fazer, mas confirmar com Rafa_gamer
+    def __str__(self):
+        return f'DATA: {self.carrinhoData}, {self.usuario} --> {self.estado}'
+
+class ItemCarrinho(models.Model):
+    carrinho   = models.ForeignKey(Carrinho, on_delete=models.CASCADE, verbose_name='ItemCarrinho')
+    produto    = models.ForeignKey(Produtos, on_delete=models.CASCADE, verbose_name='ProdutoItemCarrinho')
+    quantidade = models.PositiveIntegerField(default=1, verbose_name='QuantidadeItemCarrinho')
+    
+    def __str__(self):
+        return f'{self.produto} x {self.quantidade}'
