1045ae0c86cbc915535edfb643e86ef626fccf3f
1R10 <ryanguilherme2003@gmail.com>
2026-06-01 22:17:21 -0300

    feat: alterado o modo de variação de produtos com VariacaoProduto

--- apollo_lojinhaREST/Produtos/admin.py
+++ apollo_lojinhaREST/Produtos/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 
-from Produtos.models import Produtos
+from Produtos.models import Produtos, VariacaoProduto
 
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nomeProduto', 
+    list_display = (
+                    'nomeProduto', 
                     'descricaoProduto',
                     'precoProduto',
-                    'tamanhoProduto',
-                    'corProduto',
-                    'estoqueProduto' )
+                    )
     
     list_display_links = ('nomeProduto',)
     list_per_page      = 20
-    search_fields      = ('nomeProduto','corProduto')
+    search_fields      = ('nomeProduto',)
 
 admin.site.register(Produtos,ProdutoAdmin)
+
+class VariacaoProdutoAdmin(admin.ModelAdmin):
+    list_display = ('produto',
+                    'tamanhoProduto',
+                    'corProduto',
+                    'estoqueProduto',
+                    )
+    list_display_links = ('produto', 'tamanhoProduto', 'corProduto')
+    list_per_page      = 20
+    search_fields      = ('tamanhoProduto', 'corProduto',)
+admin.site.register(VariacaoProduto,VariacaoProdutoAdmin)
--- apollo_lojinhaREST/Produtos/models.py
+++ apollo_lojinhaREST/Produtos/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 
 class Produtos(models.Model):
+    '''Aqui será cadastrado o produto'''
     nomeProduto      = models.CharField(max_length=200, blank= False, verbose_name='Nome')
     descricaoProduto = models.TextField(verbose_name='Descrição')
     precoProduto     = models.FloatField(blank=False, verbose_name='Preço')
+
+    def __str__(self):
+        return f'{self.nomeProduto}'
+class VariacaoProduto(models.Model):
+    '''Aqui será cadastrada a variação com base em um obj de Produtos'''
+    produto           = models.ForeignKey(Produtos, on_delete=models.CASCADE, verbose_name='Produto') # provavelmente tem um produto.nomeProduto
     TAMANHOPRODUTO   = (
-        ('2p', 'PP'),
         ('p' , 'P'),
         ('m' , 'M'),
         ('g' , 'G'),
-        ('2g', 'GG'),
     )  
-    tamanhoProduto    = models.TextField(choices=TAMANHOPRODUTO, blank= False, verbose_name='Tamanho') 
-    corProduto        = models.TextField(blank= False, verbose_name='Cor')
-    estoqueProduto = models.PositiveIntegerField(default=0, verbose_name='Estoque')
+
+    tamanhoProduto     = models.TextField(default='m',choices=TAMANHOPRODUTO, blank= False, verbose_name='Tamanho') 
+    corProduto         = models.TextField(blank= False, verbose_name='Cor')
+    estoqueProduto = models.PositiveIntegerField(default=0, verbose_name='Estoque') # ver um jeito de associar com produto e somar total
 
     def __str__(self):
-        return f'{self.nomeProduto} - {self.corProduto} - {self.tamanhoProduto}'
+        return f'{self.produto.nomeProduto} - {self.corProduto} - {self.tamanhoProduto} - {self.estoqueProduto}'
