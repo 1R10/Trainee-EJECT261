fffca56aab2e98cefbb915045bef1a49ddb8ba85
1R10 <ryanguilherme2003@gmail.com>
2026-06-10 00:28:01 -0300

    feat: iniciei a modelagem do PedidoModel

--- apollo_lojinhaREST/Pedidos/models.py
+++ apollo_lojinhaREST/Pedidos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from Carrinho.models import  ItemCarrinho
 
-# Create your models here.
+# itemcarrinho possui TUDO. produto, variacao, user, carrinho... TUDO
+class PedidoModel(models.Model):
+    STATUS = (
+        ('Pend', 'Pendente'),
+        ('Apro', 'Aprovado'),
+        ('Canc', 'Cancelado')
+        )
+    PAGAMENTO = (
+        ('picx', 'Pix'),
+        ('cred', 'Crédito'),
+        ('debt', 'Débito')
+        )
+    
+    statusPedido    = models.TextField(choices=STATUS, blank=False)
+    pagamentoPedido = models.TextField(choices=PAGAMENTO, blank=False)
+    itensPedido     = models.ForeignKey(ItemCarrinho, on_delete=models.CASCADE, blank=False)
+    
+
