9c677136463f856e305d499798b8d1ee97421d89
1R10 <ryanguilherme2003@gmail.com>
2026-06-15 00:58:31 -0300

    feat: remoção de produto na escolha. n tinha sentido

--- apollo_lojinhaREST/Carrinho/serializers.py
+++ apollo_lojinhaREST/Carrinho/serializers.py
@@ -5,7 +5,7 @@ from Carrinho.validators import estado_validator,variacao_validator, quantidade_
 class CarrinhoSerializer(serializers.ModelSerializer):
     class Meta: 
         model  = Carrinho
-        fields = '__all__'
+        exclude = ['estado']
         def validate(self, dados):
             if not estado_validator(dados['estado']):
                 raise serializers.ValidationError({'estado':'Não pode adicionar produtos em carrinhos fechados.'})
